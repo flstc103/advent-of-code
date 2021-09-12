71e7f574537b2971143e9f3ba250cc964371823d
flstc103 <79709571+flstc103@users.noreply.github.com>
2021-09-12 13:27:43 +0100

    solve part 2

--- 2017/1/day1.py
+++ 2017/1/day1.py
@@ -5,12 +5,23 @@ def main():
   numbers = lines[0]
   #print(numbers)
   print(part1(numbers))
+  print(part2(numbers))
 
 def part1(originalInput):
   sum = 0
   length = len(originalInput)
   for index in range(0,length):
     if (originalInput[index] == originalInput[(index+1)%length]):
+      sum += int(originalInput[index])
+  return sum
+
+def part2(originalInput):
+  sum = 0
+  length = len(originalInput)
+  for index in range(0,length):
+    secondPosition = (index+int(length/2))%length
+    #print(secondPosition)
+    if (originalInput[index] == originalInput[secondPosition]): 
       sum += int(originalInput[index])
   return sum
 
